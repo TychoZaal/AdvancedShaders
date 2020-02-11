aaacc3725b00bb44cfcc0dfe762254858fb4336c
Tycho (CoreBank-L-10) <Tycho.Zaal@ing.com>
2020-02-11 16:42:52 +0100

    Able to get data from shader

--- AdvancedShaders/Assets/RayTracingMaster.cs
+++ AdvancedShaders/Assets/RayTracingMaster.cs
@@ -3,10 +3,12 @@
 public class RayTracingMaster : MonoBehaviour
 {
     public ComputeShader rayTracingShader;
+    public ComputeBuffer buffer;
     public Texture skyBox;
 
     private RenderTexture _target;
 
+    #region Filler
     [Tooltip("240 is Screen.width / 8. The higher the number to more details on the X axis")]
     [SerializeField]
     private int xThreads = 240;
@@ -14,6 +16,7 @@ public class RayTracingMaster : MonoBehaviour
     [Tooltip("135 is Screen.height / 8. The higher the number to more details on the Y axis")]
     [SerializeField]
     private int yThreads = 135;
+    #endregion
 
     [SerializeField]
     private Camera _camera;
@@ -32,7 +35,19 @@ public class RayTracingMaster : MonoBehaviour
         // Set the target and dispatch the compute shader
         rayTracingShader.SetTexture(0, "Result", _target);
 
+        buffer = new ComputeBuffer(48, 48);
+        int[] cols = new int[12];
+        buffer.SetData(cols);
+        rayTracingShader.SetBuffer(rayTracingShader.FindKernel("CSMain"), "colors", buffer);
+
         rayTracingShader.Dispatch(0, xThreads, yThreads, 1);
+        
+        buffer.GetData(cols);
+
+        for (int i = 0; i < cols.Length - 1; i++)
+        {
+            Debug.Log(cols[i]);
+        }
 
         // Apply new texture as post processing overlay
         Graphics.Blit(_target, destination);

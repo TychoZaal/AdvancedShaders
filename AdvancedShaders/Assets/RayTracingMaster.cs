eec07b3c4955fed7f31c708c084a20234fdc32de
Tycho (CoreBank-L-10) <Tycho.Zaal@ing.com>
2020-02-13 16:01:26 +0100

    CPU commands drawing. Added random colors and specularity.

--- AdvancedShaders/Assets/RayTracingMaster.cs
+++ AdvancedShaders/Assets/RayTracingMaster.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class RayTracingMaster : MonoBehaviour
 {
@@ -8,7 +9,9 @@ public class RayTracingMaster : MonoBehaviour
 
     private RenderTexture _target;
     private uint _currentSample = 0;
-    private Material _addMaterial; 
+    private Material _addMaterial;
+
+    public Light directionalLight;
 
     #region Filler
     [Tooltip("240 is Screen.width / 8. The higher the number to more details on the X axis")]
@@ -22,6 +25,72 @@ public class RayTracingMaster : MonoBehaviour
 
     [SerializeField]
     private Camera _camera;
+
+    public Vector2 sphereRadius = new Vector2(3.0f, 8.0f);
+    public uint maxSpheres = 100;
+    public float spherePlacementRadius = 100f;
+    private ComputeBuffer _sphereBuffer;
+
+    struct Sphere
+    {
+        public Vector3 position;
+        public float radius;
+        public Vector3 albedo;
+        public Vector3 specular;
+    }
+
+    private void OnEnable()
+    {
+        _currentSample = 0;
+        SetUpScene();
+    }
+
+    private void OnDisable()
+    {
+        if (_sphereBuffer != null)
+            _sphereBuffer.Release();
+    }
+
+    private void SetUpScene()
+    {
+        List<Sphere> spheres = new List<Sphere>();
+
+        // Add a number of random spheres
+        for (int i = 0; i < maxSpheres - 1; i++)
+        {
+            Sphere sphere = new Sphere();
+
+            // Radius and radius
+            sphere.radius = sphereRadius.x + Random.value * (sphereRadius.y - sphereRadius.x);
+            Vector2 randomPos = Random.insideUnitCircle * spherePlacementRadius;
+            sphere.position = new Vector3(randomPos.x, sphere.radius, randomPos.y);
+
+            foreach (Sphere other in spheres)
+            {
+                float minDistance = sphere.radius + other.radius;
+                if (Vector3.SqrMagnitude(sphere.position - other.position) < minDistance * minDistance)
+                {
+                    goto SkipSphere;
+                }
+            }
+
+            // Albedo and specular color
+            Color color = Random.ColorHSV();
+            bool metal = Random.value < 0.5f;
+            sphere.albedo = metal ? Vector3.zero : new Vector3(color.r, color.g, color.b);
+            sphere.specular = metal ? new Vector3(color.r, color.g, color.b) : Vector3.one * 0.7f;
+
+            // Add spheres to list
+            spheres.Add(sphere);
+
+        SkipSphere:
+            continue;
+        }
+
+        // Assign to compute buffer
+        _sphereBuffer = new ComputeBuffer(spheres.Count, 40);
+        _sphereBuffer.SetData(spheres);
+    }
 
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
@@ -74,14 +143,19 @@ public class RayTracingMaster : MonoBehaviour
         rayTracingShader.SetMatrix("_CameraInverseProjection", _camera.projectionMatrix.inverse);
         rayTracingShader.SetTexture(0, "_SkyboxTexture", skyBox);
         rayTracingShader.SetVector("_PixelOffset", new Vector2(Random.value, Random.value));
+        Vector3 l = directionalLight.transform.forward;
+        rayTracingShader.SetVector("_DirectionalLight", new Vector4(l.x, l.y, l.z, directionalLight.intensity));
+        rayTracingShader.SetBuffer(0, "_Spheres", _sphereBuffer);
     }
 
     private void Update()
     {
-        if (transform.hasChanged)
+        if (transform.hasChanged || directionalLight.transform.hasChanged)
         {
             _currentSample = 0;
             transform.hasChanged = false;
+            directionalLight.transform.hasChanged = false;
         }
+        rayTracingShader.SetFloat("_Time", Time.time);
     }
 }
